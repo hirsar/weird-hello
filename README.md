8443b8a64d13aa0885f8c64509689ceb4d8e096a
Hiren Saravana <69997752+hirsar@users.noreply.github.com>
2020-09-01 13:47:31 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+Obfuscated C program that prints `"Hello"`. Compile with `gcc weirdhello.c -w`. View code with `cat weirdhello.c`. Run with `./a.out`.
---
+++ weirdhello.c
@@ -0,0 +1,2 @@
+typedef double b;typedef int i;typedef char k;main(){b x=72101108108111;b y=1E15;b r=0;b d=1E3;
+i j=0; while(j<6){b p=(x/y-r)*d;k c=(i)p;printf("%s",&c);r+=((i)p)/d;d*=1E3;j++;}printf("\n");}
